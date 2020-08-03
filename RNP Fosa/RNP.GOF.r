817de7ba4deafce5d0003a80ca36f4da7b0ad659
bgerber123 <bgerber@uri.edu>
2020-08-03 10:28:20 -0400

    add GOF function

---
+++ RNP Fosa/RNP.GOF.r
@@ -0,0 +1,33 @@
+#Bayesian p-value for goodness of fit script.
+
+rm(list=ls())
+library(runjags)
+source("simulation study/det.matrix.func.r")
+source("RNP Fosa/multi.state.likelihood.r")
+source("RNP Fosa/GOF.r")
+
+#load most supported model and data
+load("RNP Fosa/RNP.data")
+
+#assign data to objects
+y=RNP.data[[1]] #detection history
+
+load("RNP Fosa/M1.full.out")
+fit=combine.mcmc(M1.full)
+
+
+model.type="full"
+
+#The GOF function is not very efficient due to a for loop across sites and across mcmc samples.
+#This could be sped up significantly.
+dev=GOF(fit,y,model.type)
+save(dev,file="dev.RNP.out")
+
+#plot the Predicted and observed deviances
+hist(dev$Deviance.Predicted,breaks=50,col=2)
+hist(dev$Deviance.Observed,breaks=50,add=TRUE)
+  
+#Calculate Bayesian P-vale. Small (<0.1) or large values (>0.9) indicate
+#a lack of model fit to the data
+
+length(which(dev$Deviance.Observed>dev$Deviance.Predicted))/n.mcmc
